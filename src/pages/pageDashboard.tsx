077ca9619feafca7ee45e0e394b88434ac72870e
suntouse2 <suntouse2@gmail.com>
2024-11-12 00:58:59 +0400

    anti spam

--- src/pages/pageDashboard.tsx
+++ src/pages/pageDashboard.tsx
@@ -1,87 +1,37 @@
-import Bubble from '@components/common/Bubble';
-import Container from '@components/wrappers/layouts/Container';
-import { Button } from '@mui/material';
-import { supportLink } from '@static/links';
-import { useNavigate } from 'react-router-dom';
+import Bubble from '@components/common/Bubble'
+import Container from '@components/wrappers/layouts/Container'
+import { Button } from '@mui/material'
+import { supportLink } from '@static/links'
+import { useNavigate } from 'react-router-dom'
 
 export default function PageDashboard() {
-  const navigate = useNavigate();
+	const navigate = useNavigate()
 
-  return (
-    <Container>
-      <h1 className='text-3xl my-2 font-bold text-softgray4'>Umafic ADS</h1>
-      <div className='flex gap-3 mt-5'>
-        <Button href={supportLink} target='_blank' className='!rounded-full' variant='contained'>
-          Тех.поддержка
-        </Button>
-      </div>
-      <article className='grid grid-cols-auto-fill-300 gap-5 mt-10'>
-        <Bubble onClick={() => navigate('/campaigns/direct')}>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Поиск клиентов</h1>
-          </div>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Таргет рассылка</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Пиар в чатах</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>GEO Таргет</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Услуги маркетолога</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Таргет Инвайт</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Посты в чатах</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Директ</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Антиспам бот</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Автовебинары</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-        <Bubble>
-          <div className='flex items-center justify-between mb-5'>
-            <h1 className='text-2xl'>Объявления</h1>
-          </div>
-          <p className='text-softgray3 text-xs'>В разработке...</p>
-        </Bubble>
-      </article>
-    </Container>
-  );
+	return (
+		<Container>
+			<h1 className='text-3xl my-2 font-bold text-softgray4'>Umafic ADS</h1>
+			<div className='flex gap-3 mt-5'>
+				<Button
+					href={supportLink}
+					target='_blank'
+					className='!rounded-full'
+					variant='contained'
+				>
+					Тех.поддержка
+				</Button>
+			</div>
+			<article className='grid grid-cols-auto-fill-300 gap-5 mt-10'>
+				<Bubble onClick={() => navigate('/campaigns/direct')}>
+					<div className='flex items-center justify-between mb-5'>
+						<h1 className='text-2xl'>Поиск клиентов</h1>
+					</div>
+				</Bubble>
+				<Bubble onClick={() => navigate('/antispam')}>
+					<div className='flex items-center justify-between mb-5'>
+						<h1 className='text-2xl'>Анти-спам бот</h1>
+					</div>
+				</Bubble>
+			</article>
+		</Container>
+	)
 }
--- src/static/routes.tsx
+++ src/static/routes.tsx
@@ -1,55 +1,63 @@
 /* eslint-disable react-refresh/only-export-components */
-import { lazy } from 'react';
-import PageError from '@pages/pageError';
-import PageLayout from '@components/wrappers/layouts/PageLayout';
+import PageLayout from '@components/wrappers/layouts/PageLayout'
+import PageError from '@pages/pageError'
+import { lazy } from 'react'
 
-const NotFound = lazy(() => import('@pages/page404'));
-const Dashboard = lazy(() => import('@pages/pageDashboard'));
-const Balance = lazy(() => import('@pages/pageBalance'));
-const DirectCampaign = lazy(() => import('@pages/pageDirectCampaign'));
-const ComposeDirectCampaign = lazy(() => import('@pages/pageComposeDirectCampaign'));
-const Chat = lazy(() => import('@pages/pageChat'));
+const NotFound = lazy(() => import('@pages/page404'))
+const Dashboard = lazy(() => import('@pages/pageDashboard'))
+const Balance = lazy(() => import('@pages/pageBalance'))
+const DirectCampaign = lazy(() => import('@pages/pageDirectCampaign'))
+const ComposeDirectCampaign = lazy(
+	() => import('@pages/pageComposeDirectCampaign')
+)
+const Chat = lazy(() => import('@pages/pageChat'))
+const Spam = lazy(() => import('@pages/pageAntiSpam'))
 
 export const routes = [
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-  {
-    path: '/',
-    element: <PageLayout />,
-    errorElement: <PageError />,
-    children: [
-      {
-        name: 'dashboard',
-        path: '',
-        element: <Dashboard />,
-      },
-      {
-        name: 'payment page',
-        path: 'balance',
-        element: <Balance />,
-      },
-      {
-        name: 'direct campaign',
-        path: 'campaigns/direct',
-        element: <DirectCampaign />,
-      },
-      {
-        name: 'direct campaign create',
-        path: 'campaigns/direct/create',
-        element: <ComposeDirectCampaign />,
-      },
-      {
-        name: 'direct campaign edit',
-        path: 'campaigns/direct/edit',
-        element: <ComposeDirectCampaign />,
-      },
-      {
-        name: 'chat',
-        path: 'chat/:campaignId/:directId?',
-        element: <Chat />,
-      },
-    ],
-  },
-];
+	{
+		path: '*',
+		element: <NotFound />,
+	},
+	{
+		path: '/',
+		element: <PageLayout />,
+		errorElement: <PageError />,
+		children: [
+			{
+				name: 'dashboard',
+				path: '',
+				element: <Dashboard />,
+			},
+			{
+				name: 'payment page',
+				path: 'balance',
+				element: <Balance />,
+			},
+			{
+				name: 'direct campaign',
+				path: 'campaigns/direct',
+				element: <DirectCampaign />,
+			},
+			{
+				name: 'direct campaign create',
+				path: 'campaigns/direct/create',
+				element: <ComposeDirectCampaign />,
+			},
+			{
+				name: 'direct campaign edit',
+				path: 'campaigns/direct/edit',
+				element: <ComposeDirectCampaign />,
+			},
+			{
+				name: 'chat',
+				path: 'chat/:campaignId/:directId?',
+				element: <Chat />,
+			},
+			{
+				name: 'antispam',
+				path: 'antispam',
+				element: <Spam />,
+			},
+		],
+	},
+]
